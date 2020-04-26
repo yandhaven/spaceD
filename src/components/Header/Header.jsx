88570d53f6eac1074f4849a9ca586581f260d43f
yandhaven <yandhaven@yandhavens-iMac.local>
2020-04-26 16:56:05 +0300

    some changes in components header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import modules from '../Header/Header.module.css'
+
+const Header =()=>{
+    return <header className={modules.header}>
+        <img src='emblemURL' />
+    </header>
+}
+
+export default Header;
